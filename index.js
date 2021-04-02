cca3cf8807cdca38c87fbebc5f82546397caea95
Daniel Phan <dannyphan@comcast.net>
2021-04-01 22:55:12 -0700

    Added questions for user input

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+console.log(inquirer)
+
+const fs = require("fs");
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ([
+    {
+        type: "input",
+        name: "Project Title",
+        message: "What is the title of your project?",
+    },
+    {
+        type: "input",
+        name: "Please describe your project"
+    }
+]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
